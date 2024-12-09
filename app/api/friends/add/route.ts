cafd1b358267908cc734e10ea8aaab80eec36d7f
NiloyChowdhury <iamvrniloyc@gmail.com>
2024-12-09 09:29:49 +0530

    Debugging Perpose

--- app/api/friends/add/route.ts
+++ app/api/friends/add/route.ts
@@ -28,31 +28,31 @@ try {
     if(idToAdd===session.user.id){
         return new Response('You cannot add yourself as a friend',{status:400})
     }
-
-    pusherServer.trigger(toPusherKey(`user:${idToAdd}:incoming_friend_requests`),'incoming_friend_requests',{
-        senderId: session.user.id,
-        senderEmail: session.user.email
-    })
-
-
+    
+    
     //check for already added user
-
+    
     const isAlreadyAdded = await fetchRedis('sismember',`user:${idToAdd}:incoming_friend_requests`,session.user.id) as 0|1
-     
+    
     if(isAlreadyAdded){
         return new Response('Already added this user',{status:400})
     }
-
+    
     //check for already added user
-
+    
     const isAlreadyFriends = await fetchRedis('sismember',`user:${session.user.id}:incoming_friend_requests`,idToAdd) as 0|1
-     
+    
     if(isAlreadyFriends){
         return new Response('Already added this user',{status:400})
     }
-
+    
+        pusherServer.trigger(toPusherKey(`user:${idToAdd}:incoming_friend_requests`),'incoming_friend_requests',{
+            senderId: session.user.id,
+            senderEmail: session.user.email
+        })
+    
     await db.sadd(`user:${idToAdd}:incoming_friend_requests`,session.user.id)
-
+    
     return new Response('OK')
 } catch (error) {
     if(error instanceof z.ZodError){
